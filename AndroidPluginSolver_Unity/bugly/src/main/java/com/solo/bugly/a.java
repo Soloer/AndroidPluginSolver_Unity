23fb3c152f5cdaa146d856a77857b497df4adb24
Soloer <yangshousen@gmail.com>
2017-11-12 23:34:39 +0800

    no message

---
+++ AndroidPluginSolver_Unity/bugly/src/main/java/com/solo/bugly/a.java
@@ -0,0 +1,14 @@
+package com.solo.bugly;
+import com.tencent.bugly.Bugly;
+
+/**
+ * Created by soloeryoung on 2017/11/12.
+ */
+
+public class a {
+
+    public void a ()
+    {
+//        Bugly.init();
+    }
+}
